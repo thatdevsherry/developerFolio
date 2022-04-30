55092448e24ab76ac0f6d12834100444d41ff900
Shehriyar Qureshi <thatdevsherry@gmail.com>
2022-04-30 22:22:49 +0500

    always use light mode

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -22,8 +22,8 @@ import {useLocalStorage} from "../hooks/useLocalStorage";
 import "./Main.scss";
 
 const Main = () => {
-  const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
+  // const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+  const [isDark, setIsDark] = useLocalStorage("isDark", false);
   const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
     useState(true);
 
