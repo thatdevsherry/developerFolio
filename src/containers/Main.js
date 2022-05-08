a49148dd3caa051be4be3e080ca47cf824c4ce9d
Shehriyar Qureshi <thatdevsherry@gmail.com>
2022-05-08 12:01:21 +0500

    fix(): switch b/w dark/light mode

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -23,7 +23,7 @@ import "./Main.scss";
 
 const Main = () => {
   const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref);
+  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
   const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
     useState(true);
 
